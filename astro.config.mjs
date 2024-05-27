d44885f654687788f666552cb8f56ecdfc2cfd83
kuniyan <57828407+kuni-0930@users.noreply.github.com>
2024-05-28 02:17:49 +0900

    test 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kuni-0930.github.io/",
-  outDir: "./docs",
+  //outDir: "./docs",
   base: "/Kuni_Tool",
   integrations: [
     starlight({
